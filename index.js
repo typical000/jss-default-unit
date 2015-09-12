70220e4d68058856cc1ad7750a69117b2d93cdbf
Oleg Slobodskoi <oleg008@gmail.com>
2015-09-12 21:28:51 +0200

    add more numeric props

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 // Don't automatically add 'px' to these possibly-unitless properties.
 // Borrowed from jquery.
 var cssNumber = {
+    'animation-iteration-count': true,
+    'box-ordinal-group': true,
     'column-count': true,
     'fill-opacity': true,
     'flex': true,
     'flex-grow': true,
+    'flex-order': true,
     'flex-shrink': true,
     'font-weight': true,
     'line-height': true,
     'opacity': true,
     'order': true,
     'orphans': true,
+    'stop-opacity': true,
+    'tab-size': 1,
     'widows': true,
     'z-index': true,
     'zoom': true
